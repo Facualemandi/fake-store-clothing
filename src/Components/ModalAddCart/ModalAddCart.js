d66d73f9903560d3b034bbc07a6d62ad9db1a56b
Facundo Alemandi <facuinstituto10@gmail.com>
2022-06-22 13:05:20 -0300

    Terminando el desktopd e los modales, falta el de like

--- src/Components/ModalAddCart/ModalAddCart.js
+++ src/Components/ModalAddCart/ModalAddCart.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import './ModalAddCart.css';
-import { FcApproval } from 'react-icons/fc';
+import React from "react";
+import "./ModalAddCart.css";
+import { FcApproval } from "react-icons/fc";
 
 const ModalAddCart = () => {
-  
-
-
   return (
-     <> 
-        <section className='modal'>
-                <section className='section_modal'>
-                   
-                    <FcApproval className='icon_check'/>
-                    <p>Producto Agregado</p>
-                </section>
+    <>
+      <section className="modal">
+        <section className="section_section_modal">
+          <section className="section_modal">
+            <FcApproval className="icon_check" />
+            <p>Producto Agregado</p>
+          </section>
         </section>
-     
-     </>
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default ModalAddCart
+export default ModalAddCart;
